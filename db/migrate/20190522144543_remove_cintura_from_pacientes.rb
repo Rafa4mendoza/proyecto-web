20b6b2198400aeb8e6b78df8e5edeeef4a94f246
Rafael Mendoza <rafael.mendozag22@gmail.com>
2019-05-22 15:02:16 -0500

    migracion

--- db/migrate/20190522144543_remove_cintura_from_pacientes.rb
+++ db/migrate/20190522144543_remove_cintura_from_pacientes.rb
@@ -1,6 +1,10 @@
 
 class RemoveCinturaFormPacientes < ActiveRecord::Migration[5.0]
-    def change
-      remove_column :pacientes, :cintura, :float
-    end
+    def up
+        remove_column :pacientes, :cintura, :float
+      end
+    
+      def down
+        add_column :pacientes, :cintura, :float
+      end
   end
